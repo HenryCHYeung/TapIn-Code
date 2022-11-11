9362f165435ea9c8cb82c8ad5b0a4615f9bf346c
OrtegaIsaac <117954684+OrtegaIsaac@users.noreply.github.com>
2022-11-10 19:45:58 -0500

    Add files via upload

---
+++ tapin.sql
@@ -0,0 +1,64 @@
+CREATE TABLE students
+(
+	studentID		INT				PRIMARY KEY, 
+	stuFirstName	VARCHAR(50)		NOT NULL, 
+	stuLastName		VARCHAR(50)		NOT NULL,
+    studentEmail		varchar(100) 	NOT NUll
+);
+
+CREATE TABLE professors
+(
+	professorID		INT				PRIMARY KEY, 
+	profFirstName	VARCHAR(50)		NOT NULL, 
+	profLastName	VARCHAR(50)		NOT NULL,
+	profEmail		varchar(100) 	NOT NUll
+);
+
+CREATE TABLE attendance
+(
+	attendID		INT				PRIMARY KEY		AUTO_INCREMENT, 
+	attendClass		VARCHAR(50)		NOT NULL, 
+	attendDate		VARCHAR(50)		NOT NULL,
+    attendTime		VARCHAR(50)		NOT NULL,
+    studentID		INT				NOT NULL,
+	profID			INT				NOT NULL,
+    CONSTRAINT attendance_fk_studentid FOREIGN KEY (studentID)
+		REFERENCES students (studentID),
+        
+	CONSTRAINT attendance_fk_profid FOREIGN KEY (profID)
+		REFERENCES professors (profID)
+);
+
+CREATE TABLE password
+(
+	passwordID		VARCHAR(100)		PRIMARY KEY,
+	studentID		INT					NOT NULL,
+	profID			INT					NOT NULL,
+    studentEmail	varchar(100) 		NOT NUll,
+	profEmail		varchar(100) 		NOT NUll,
+    CONSTRAINT password_fk_studentid FOREIGN KEY (studentID)
+		REFERENCES students (studentID),
+        
+	CONSTRAINT password_fk_profid FOREIGN KEY (profID)
+		REFERENCES professors (profID),
+        
+	CONSTRAINT password_fk_studentemail FOREIGN KEY (studentEmail)
+		REFERENCES students (studentEmail),
+        
+	CONSTRAINT password_fk_profemail FOREIGN KEY (profEmail)
+		REFERENCES professors (profEmail)
+);
+
+INSERT INTO students VALUES
+(1281205,'Isaac','Ortega','ortega@nyit.edu'),
+(1277182,'Christian','Pascal','csinoagp@nyit.edu'),
+(1187413,"O'Sean",'Blagrove','oblagrov@nyit.edu'),
+(1293960,'Henry','Yeung','');
+
+INSERT INTO professors VALUES
+('1','Wenja','Li','wli20@nyit.edu');
+
+
+
+
+
