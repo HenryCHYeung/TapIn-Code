7c6da2711f83330efe255d40675959fdec9bc431
Henry Yeung <yeungchihang22@gmail.com>
2022-11-13 15:21:23 -0500

    Added Node.js functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+
 function swapBody(current, target) {
     document.getElementById(current).style.display = 'none';
     document.getElementById(target).style.display = 'block';
@@ -34,13 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
         setFormMessage(loginForm, "error", "Invalid ID/password");
     });
 
-    createAccountForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        // Create Account
-
-        setFormMessage(createAccountForm, "error", "Invalid ID/email address/password");
-    });
 
     forgotPasswordForm.addEventListener("submit", e => {
         e.preventDefault();
