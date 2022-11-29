e76a1e11f60a03062e857b5d8adc1fdd7896eb7a
OrtegaIsaac <117954684+OrtegaIsaac@users.noreply.github.com>
2022-11-29 18:44:31 -0500

    Changed table from csci 318 to attendance
    Reverted back to attendance for the table. Includes the prof id, class id ( class code), attend ID, student id, date, and time.
    Some test values are also present with the accommodating data.

--- tapin.sql
+++ tapin.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS students
 	studentID		INT			PRIMARY KEY, 
 	stuFirstName		VARCHAR(50)		NOT NULL, 
 	stuLastName		VARCHAR(50)		NOT NULL,
-	studentEmail		varchar(50) 		NOT NUll,
+	studentEmail		varchar(50) 		NOT NULL,
 	studentPassword		varchar(50)		NOT NULL
 );
 
@@ -12,16 +12,21 @@ CREATE TABLE IF NOT EXISTS professors
 	profID			INT			PRIMARY KEY, 
 	profFirstName		VARCHAR(50)		NOT NULL, 
 	profLastName		VARCHAR(50)		NOT NULL,
-	profEmail		varchar(50) 		NOT NUll,
+	profEmail		varchar(50) 		NOT NULL,
 	profPassword		varchar(50)		NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS csci318
+CREATE TABLE IF NOT EXISTS attendance
 (
+	profID			INT			NOT NULL,
+	classID			varchar(20)		NOT NULL,
 	attendID		INT			IDENTITY(1,1)   PRIMARY KEY, 
 	studentID		INT			NOT NULL,
 	attendDate		DATE			NOT NULL,
  	attendTime		TIME,
+	
+	CONSTRAINT attendance_fk_profid FOREIGN KEY (profID)
+		REFERENCES professors (profID),
 	
 	CONSTRAINT attendance_fk_studentid FOREIGN KEY (studentID)
 		REFERENCES students (studentID)
@@ -38,9 +43,9 @@ INSERT OR REPLACE INTO students VALUES
 INSERT OR REPLACE INTO professors VALUES
 (1,'Wenjia','Li','wli20@nyit.edu','passwordLi');
 
-INSERT OR REPLACE INTO csci318 VALUES
-(1,1281205,'2022-11-18', '09:40:24'),
-(2,1277182,'2022-11-18', '09:35:12'),
-(3,1187413,'2022-11-18', '09:38:54'),
-(4,1293960,'2022-11-18', '09:30:01'),
-(5,1256578,'2022-11-18', '09:45:59');
+INSERT OR REPLACE INTO attendance VALUES
+(1,'CSCI318',1,1281205,'2022-11-18', '09:40:24'),
+(1,'CSCI318',2,1277182,'2022-11-18', '09:35:12'),
+(1,'CSCI318',3,1187413,'2022-11-18', '09:38:54'),
+(1,'CSCI318',4,1293960,'2022-11-18', '09:30:01'),
+(1,'CSCI318',5,1256578,'2022-11-18', '09:45:59');
