1c4c4922005f966e016201ec75487dadb2f76d8a
Henry Yeung <yeungchihang22@gmail.com>
2022-11-29 15:29:23 -0500

    Added the basics of View Attendance

--- professorlanding.js
+++ professorlanding.js
@@ -1,7 +1,30 @@
-function displayModal() {
-    document.getElementById("newClassModal").style.display = "block";
+function showElement(courseName) {
+    document.getElementById(courseName).style.display = "block";
 }
 
-function closeModal() {
-    document.getElementById("newClassModal").style.display = "none";
+function hideElement(courseName) {
+    document.getElementById(courseName).style.display = "none";
 }
+
+function switchButtonView() {
+    var button = document.getElementById("showAttend");
+    var options = document.getElementById("viewOptions");
+    var studentView = document.getElementById("studentView");
+    var dateView = document.getElementById("dateView");
+
+    if (button.innerHTML == "View Attendance") {
+        button.innerHTML = "Hide Attendance";
+        options.style.display = "block";
+    } else if (button.innerHTML == "Hide Attendance") {
+        button.innerHTML = "View Attendance";
+        options.style.display = "none";
+        studentView.style.display = "none";
+        dateView.style.display = "none";
+    }
+}
+
+function toggleView(show, hide) {
+    document.getElementById(show).style.display = "block";
+    document.getElementById(hide).style.display = "none";
+}
+
